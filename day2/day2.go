797ee65cf13d6ff22e0f760c03c2f83307a85dea
chromohh <sindre.he@hotmail.com>
2021-12-09 13:32:49 +0100

    day2 p1 finished

--- day2/day2.go
+++ day2/day2.go
@@ -1,5 +1,27 @@
 package main
 
-func main(){
+import(
+	u "chromo.com/aocgolang/utils"
+    "fmt"
+	"regexp"
+	"strconv"
+)
 
+const PATH = "../day2/input.txt"
+var input, _ = u.ParseFileToStringArr(PATH)
+
+func main() {
+	cordIX, cordY := 0, 0
+	r, _ := regexp.Compile("[0-9]")
+
+	for _, element := range input{
+		s := r.FindString(element)
+		n, _ := strconv.Atoi(s);
+			switch element[:1]{
+		    case "u": cordIX -= n
+		    case "f": cordY += n
+			case "d": cordIX += n
+		}
+	}
+	fmt.Printf("\n %s %d", "PART1: Result " , (cordIX*cordY))
 }
